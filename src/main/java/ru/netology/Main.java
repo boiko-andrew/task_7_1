0f42afea4d45e046046d35ab4ac62418d3267423
Andrew Boiko <boiko_andrew@mail.ru>
2023-11-06 10:27:38 +0300

    Modify Request and Server classes to get parameters from query

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -6,13 +6,14 @@ public class Main {
     private static final int SERVER_PORT = 9999;
     private static final int THREAD_POOL_SIZE = 64;
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         Server server = new Server(SERVER_PORT, THREAD_POOL_SIZE);
 
-        // adding handlers (code for processing)
+        // add handlers
         server.addHandler("GET", "/messages", (request, responseStream) -> {
             try {
-                server.responseWithoutContent(responseStream, "404", "Not Found");
+                server.responseWithoutContent(responseStream, "200", "OK");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -26,6 +27,5 @@ public class Main {
 
         // Start
         server.start();
-        System.out.println("Server started.");
     }
 }
